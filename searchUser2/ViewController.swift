847ae7bbf6e392f0fca79a28ed67ca6543d7b561
serenalai <serena.lai@honestbee.com>
2019-07-13 12:27:01 +0800

    Initial Commit

---
+++ searchUser2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  searchUser2
+//
+//  Created by Serena on 2019/7/13.
+//  Copyright © 2019 Serena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
